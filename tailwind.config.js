360d79e59ff53014a10f91054c8b4913807e6653
felix <felix@gmail.com>
2021-12-25 15:49:19 -0300

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["index.html","./src/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+/*//lo que deberia de salirme
+
+module.exports = {
+  purge: [],
+  darkmode: false, //or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+*/
